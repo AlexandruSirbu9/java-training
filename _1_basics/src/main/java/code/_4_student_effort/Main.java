f7f0f194de108cd73520bc4b8c9a40ac49f95399
Alexandru-Iulian Sirbu <asirbu086@gmail.com>
2022-04-04 13:23:13 +0300

    FizzBuzz

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -6,32 +6,4 @@ public class Main {
 
 
   }
-  public class Challenge1 {
-
-    public  void run() {
-      for (int i = 1; i <= 100; i++ ){
-
-        if ( i % 7 == 0){
-          System.out.println("Rizz");
-        }
-        else if ( i % 11 == 0){
-          System.out.println("Jazz");
-        }
-        else if ( i % 3 == 0 && i % 5 == 0) {
-          System.out.println("FizzBUzz");
-        }
-        else if ( i % 3 == 0) {
-          System.out.println("Fizz");
-        }
-        else if ( i % 5 == 0) {
-          System.out.println("Buzz");
-        }
-        else {
-          System.out.println(i);
-        }
-      }
-    }
-  }
-
-
 }
